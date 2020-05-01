be03339ec37745f812fd0661c486b368afd83482
Luis Antonio Parrado <luisprmat@gmail.com>
2020-05-01 00:37:14 -0500

    Add single view for post

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@home');
+Route::get('blog/{id}', 'PostController@show');
 
 Route::middleware('auth')
     ->namespace('Admin')
