e192846284b44d8e1b054425a4db6899977c6269
Luis Antonio Parrado <luisprmat@gmail.com>
2020-05-04 23:12:31 -0500

    Implement pagination links

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-        $posts = Post::published()->get();
+        $posts = Post::published()->paginate(2);
 
         return view('welcome', compact('posts'));
     }
