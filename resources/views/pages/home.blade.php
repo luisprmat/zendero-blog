49cc2cccd977bbbfa4638b1520f8e2ae988fdfdb
Luis Antonio Parrado <luisprmat@gmail.com>
2020-05-09 20:07:53 -0500

    Reuse views to home and single-post

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,51 +7,19 @@
     @endif
         @foreach ($posts as $post)
             <article class="post">
-                @if ($post->photos->count() === 1)
-                    <figure><img src="{{ asset('storage/' .$post->photos->first()->url) }}" alt="" class="img-responsive"></figure>
-                @elseif($post->photos->count() > 1)
-                    <div class="gallery-photos" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 464 }'>
-                        @foreach ($post->photos->take(4) as $photo)
-                            <figure class="grid-item grid-item--height2">
-                                @if ($loop->iteration === 4)
-                                    <div class="overlay">{{ $post->photos->count() }} Fotos</div>
-                                @endif
-                                <img src="{{ url('storage/'.$photo->url) }}" alt="" class="img-responsive">
-                            </figure>
-                        @endforeach
-                   </div>
-                @elseif($post->iframe)
-                    <div class="video">
-                        {!! $post->iframe !!}
-                    </div>
-                @endif
+
+                @include($post->viewType('home'))
+
                 <div class="content-post">
-                    <header class="container-flex space-between">
-                        <div class="date">
-                            <span class="c-gris">
-                                {{ $post->published_at->format('M d') }} / {{ $post->owner->name }}
-                            </span>
-                        </div>
-                        <div class="post-category">
-                            <span class="category">
-                                <a href="{{ route('categories.show', $post->category) }}">
-                                    {{ optional($post->category)->name }}
-                                </a>
-                            </span>
-                        </div>
-                    </header>
+                    @include('posts.header')
                     <h1>{{ $post->title }}</h1>
                     <div class="divider"></div>
                     <p>{{ $post->excerpt }}</p>
                     <footer class="container-flex space-between">
                         <div class="read-more">
-                            <a href="blog/{{ $post->url }}" class="text-uppercase c-green">leer más</a>
+                            <a href="{{ route('posts.show', $post) }}" class="text-uppercase c-green">leer más</a>
                         </div>
-                        <div class="tags container-flex">
-                            @foreach ($post->tags as $tag)
-                                <span class="tag c-gris"><a href="{{ route('tags.show', $tag) }}">#{{ $tag->name }}</a></span>
-                            @endforeach
-                        </div>
+                        @include('posts.tags')
                     </footer>
                 </div>
             </article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,26 +6,11 @@
 @section('content')
 <section class="posts container">
     <article class="post">
-        @if ($post->photos->count() === 1)
-            <figure><img src="{{ url("storage/{$post->photos->first()->url}") }}" alt="image" class="img-responsive"></figure>
-        @elseif($post->photos->count() > 1)
-            @include('posts.carousel')
-        @elseif($post->iframe)
-            <div class="video">
-                {!! $post->iframe !!}
-            </div>
-        @endif
+
+        @include($post->viewType())
+
         <div class="content-post">
-            <header class="container-flex space-between">
-                <div class="date">
-                    <span class="c-gris">{{ optional($post->published_at)->format('M d') }}</span>
-                </div>
-                @if ($post->category)
-                    <div class="post-category">
-                        <span class="category">{{ $post->category->name }}</span>
-                    </div>
-                @endif
-            </header>
+            @include('posts.header')
             <h1>{{ $post->title }}</h1>
             <div class="divider"></div>
             <div class="image-w-text">
@@ -34,11 +19,7 @@
 
             <footer class="container-flex space-between">
                 @include('partials.social-links', ['description' => $post->title])
-                <div class="tags container-flex">
-                    @foreach ($post->tags as $tag)
-                        <span class="tag c-gris text-capitalize">#{{ $tag->name }}</span>
-                    @endforeach
-                </div>
+                @include('posts.tags')
             </footer>
             <div class="comments">
                 <div class="divider"></div>
